9141591b63a0101ca5df715b4ed6055d3c2f2d29
hoangtan <hoangtan79bk@gmail.com>
2025-01-25 17:36:57 +0700

    change data 1

--- pages/giapha/familyData.js
+++ pages/giapha/familyData.js
@@ -1,116 +1,336 @@
 // familyData.js
-
 export const familyData = {
     nodes: {
-        // Đời 1 (1 người)
+        // Đời 1 (Ông Tổ)
         N1: {
             id: 'N1',
-            name: 'Nguyễn Văn A',
+            name: 'Nguyễn Văn Minh',
             birthYear: 1900,
             generation: 1,
             spouses: [{
                 id: 'SP1',
-                name: 'Trần Thị B',
+                name: 'Trần Thị Hoa',
                 birthYear: 1902,
                 marriageYear: 1920
             }]
         },
-        // Đời 2 (3 người)
+
+        // Đời 2 (Con của Ông Tổ - 4 người)
         N2: {
             id: 'N2',
-            name: 'Nguyễn Văn C',
+            name: 'Nguyễn Văn An',
             birthYear: 1925,
             generation: 2,
             motherId: 'SP1',
             spouses: [{
                 id: 'SP2',
-                name: 'Lê Thị D',
+                name: 'Lê Thị Mai',
                 birthYear: 1928,
                 marriageYear: 1945
             }]
         },
         N3: {
             id: 'N3',
-            name: 'Nguyễn Thị E',
+            name: 'Nguyễn Thị Liên',
             birthYear: 1927,
-            generation: 2,
-            motherId: 'SP1'
-        },
-        N4: {
-            id: 'N4',
-            name: 'Nguyễn Văn F',
-            birthYear: 1930,
             generation: 2,
             motherId: 'SP1',
             spouses: [{
                 id: 'SP3',
-                name: 'Phạm Thị G',
+                name: 'Phạm Văn Đức',
+                birthYear: 1925,
+                marriageYear: 1944
+            }]
+        },
+        N4: {
+            id: 'N4',
+            name: 'Nguyễn Văn Bình',
+            birthYear: 1930,
+            generation: 2,
+            motherId: 'SP1',
+            spouses: [{
+                id: 'SP4',
+                name: 'Hoàng Thị Lan',
                 birthYear: 1932,
                 marriageYear: 1950
             }]
         },
-        // Đời 3 (8 người)
         N5: {
             id: 'N5',
-            name: 'Nguyễn Văn H',
-            birthYear: 1950,
-            generation: 3,
-            motherId: 'SP2'
+            name: 'Nguyễn Văn Cường',
+            birthYear: 1932,
+            generation: 2,
+            motherId: 'SP1',
+            spouses: [{
+                id: 'SP5',
+                name: 'Vũ Thị Hương',
+                birthYear: 1935,
+                marriageYear: 1952
+            }]
         },
+
+        // Đời 3 (Cháu của Ông Tổ - 12 người)
+        // Con của N2
         N6: {
             id: 'N6',
-            name: 'Nguyễn Thị I',
-            birthYear: 1952,
-            generation: 3,
-            motherId: 'SP2'
-        },
-        N7: {
-            id: 'N7',
-            name: 'Nguyễn Văn K',
-            birthYear: 1953,
+            name: 'Nguyễn Văn Dũng',
+            birthYear: 1950,
             generation: 3,
             motherId: 'SP2',
             spouses: [{
-                id: 'SP4',
-                name: 'Hoàng Thị L',
-                birthYear: 1955,
-                marriageYear: 1975
+                id: 'SP6',
+                name: 'Trần Thị Thu',
+                birthYear: 1952,
+                marriageYear: 1970
+            }]
+        },
+        N7: {
+            id: 'N7',
+            name: 'Nguyễn Thị Hà',
+            birthYear: 1952,
+            generation: 3,
+            motherId: 'SP2',
+            spouses: [{
+                id: 'SP7',
+                name: 'Lý Văn Nam',
+                birthYear: 1950,
+                marriageYear: 1971
             }]
         },
         N8: {
             id: 'N8',
-            name: 'Nguyễn Văn M',
-            birthYear: 1951,
+            name: 'Nguyễn Văn Em',
+            birthYear: 1955,
             generation: 3,
-            motherId: 'SP3'
+            motherId: 'SP2'
         },
+
+        // Con của N3
         N9: {
             id: 'N9',
-            name: 'Nguyễn Thị N',
+            name: 'Phạm Văn Tuấn',
+            birthYear: 1951,
+            generation: 3,
+            motherId: 'SP3',
+            spouses: [{
+                id: 'SP8',
+                name: 'Ngô Thị Thúy',
+                birthYear: 1953,
+                marriageYear: 1972
+            }]
+        },
+        N10: {
+            id: 'N10',
+            name: 'Phạm Thị Thủy',
             birthYear: 1953,
             generation: 3,
             motherId: 'SP3'
         },
-        N10: {
-            id: 'N10',
-            name: 'Nguyễn Văn O',
-            birthYear: 1955,
-            generation: 3,
-            motherId: 'SP3'
-        },
+
+        // Con của N4
         N11: {
             id: 'N11',
-            name: 'Nguyễn Văn P',
-            birthYear: 1957,
+            name: 'Nguyễn Văn Phong',
+            birthYear: 1955,
             generation: 3,
-            motherId: 'SP3'
+            motherId: 'SP4',
+            spouses: [{
+                id: 'SP9',
+                name: 'Đặng Thị Hồng',
+                birthYear: 1957,
+                marriageYear: 1975
+            }]
         },
         N12: {
             id: 'N12',
-            name: 'Nguyễn Thị Q',
-            birthYear: 1959,
+            name: 'Nguyễn Thị Quỳnh',
+            birthYear: 1957,
             generation: 3,
-            motherId: 'SP3'
+            motherId: 'SP4',
+            spouses: [{
+                id: 'SP10',
+                name: 'Trịnh Văn Long',
+                birthYear: 1955,
+                marriageYear: 1976
+            }]
+        },
+        N13: {
+            id: 'N13',
+            name: 'Nguyễn Văn Khang',
+            birthYear: 1960,
+            generation: 3,
+            motherId: 'SP4'
+        },
+
+        // Con của N5
+        N14: {
+            id: 'N14',
+            name: 'Nguyễn Văn Mạnh',
+            birthYear: 1958,
+            generation: 3,
+            motherId: 'SP5',
+            spouses: [{
+                id: 'SP11',
+                name: 'Lê Thị Nga',
+                birthYear: 1960,
+                marriageYear: 1978
+            }]
+        },
+        N15: {
+            id: 'N15',
+            name: 'Nguyễn Thị Ngọc',
+            birthYear: 1960,
+            generation: 3,
+            motherId: 'SP5'
+        },
+
+        // Đời 4 (Chắt của Ông Tổ - 15 người)
+        // Con của N6
+        N16: {
+            id: 'N16',
+            name: 'Nguyễn Văn Hiếu',
+            birthYear: 1975,
+            generation: 4,
+            motherId: 'SP6',
+            spouses: [{
+                id: 'SP12',
+                name: 'Phạm Thị Loan',
+                birthYear: 1978,
+                marriageYear: 1995
+            }]
+        },
+        N17: {
+            id: 'N17',
+            name: 'Nguyễn Thị Hương',
+            birthYear: 1978,
+            generation: 4,
+            motherId: 'SP6'
+        },
+
+        // Con của N7
+        N18: {
+            id: 'N18',
+            name: 'Lý Văn Hùng',
+            birthYear: 1976,
+            generation: 4,
+            motherId: 'SP7',
+            spouses: [{
+                id: 'SP13',
+                name: 'Trần Thị Mai',
+                birthYear: 1979,
+                marriageYear: 1997
+            }]
+        },
+
+        // Con của N9
+        N19: {
+            id: 'N19',
+            name: 'Phạm Văn Minh',
+            birthYear: 1977,
+            generation: 4,
+            motherId: 'SP8'
+        },
+        N20: {
+            id: 'N20',
+            name: 'Phạm Thị Linh',
+            birthYear: 1980,
+            generation: 4,
+            motherId: 'SP8'
+        },
+
+        // Con của N11
+        N21: {
+            id: 'N21',
+            name: 'Nguyễn Văn Tùng',
+            birthYear: 1980,
+            generation: 4,
+            motherId: 'SP9',
+            spouses: [{
+                id: 'SP14',
+                name: 'Hoàng Thị Thảo',
+                birthYear: 1982,
+                marriageYear: 2000
+            }]
+        },
+        N22: {
+            id: 'N22',
+            name: 'Nguyễn Thị Trang',
+            birthYear: 1982,
+            generation: 4,
+            motherId: 'SP9'
+        },
+
+        // Con của N12
+        N23: {
+            id: 'N23',
+            name: 'Trịnh Văn Đạt',
+            birthYear: 1981,
+            generation: 4,
+            motherId: 'SP10'
+        },
+        N24: {
+            id: 'N24',
+            name: 'Trịnh Thị Thu',
+            birthYear: 1983,
+            generation: 4,
+            motherId: 'SP10'
+        },
+
+        // Con của N14
+        N25: {
+            id: 'N25',
+            name: 'Nguyễn Văn Thắng',
+            birthYear: 1983,
+            generation: 4,
+            motherId: 'SP11',
+            spouses: [{
+                id: 'SP15',
+                name: 'Vũ Thị Hoa',
+                birthYear: 1985,
+                marriageYear: 2003
+            }]
+        },
+
+        // Đời 5 (Chút của Ông Tổ - 10 người)
+        // Con của N16
+        N26: {
+            id: 'N26',
+            name: 'Nguyễn Văn Nam',
+            birthYear: 2000,
+            generation: 5,
+            motherId: 'SP12'
+        },
+        N27: {
+            id: 'N27',
+            name: 'Nguyễn Thị Nhung',
+            birthYear: 2002,
+            generation: 5,
+            motherId: 'SP12'
+        },
+
+        // Con của N18
+        N28: {
+            id: 'N28',
+            name: 'Lý Văn Quân',
+            birthYear: 2001,
+            generation: 5,
+            motherId: 'SP13'
+        },
+
+        // Con của N21
+        N29: {
+            id: 'N29',
+            name: 'Nguyễn Văn Hải',
+            birthYear: 2005,
+            generation: 5,
+            motherId: 'SP14'
+        },
+        N30: {
+            id: 'N30',
+            name: 'Nguyễn Thị Thùy',
+            birthYear: 2007,
+            generation: 5,
+            motherId: 'SP14'
         }
     }
 }
--- pages/giapha/index.vue
+++ pages/giapha/index.vue
@@ -1,5 +1,6 @@
+<!-- FamilyTreeLayout.vue -->
 <template>
-  <div class="tree-wrapper">
+  <div class="tree-container">
     <!-- Search box -->
     <div class="search-box">
       <input
@@ -11,15 +12,24 @@
       <button v-if="searchQuery" @click="clearSearch" class="clear-btn">×</button>
     </div>
 
-    <v-network-graph
-        :nodes="nodes"
-        :edges="edges"
-        :layouts="layouts"
-        :configs="configs"
-        @node-click="handleNodeClick"
-    />
+    <div class="zoom-controls">
+      <button @click="zoomIn">+</button>
+      <button @click="zoomOut">-</button>
+    </div>
+
+    <div class="scroll-container" :style="{ transform: `scale(${zoom})` }">
+      <v-network-graph
+          :nodes="filteredNodes"
+          :edges="filteredEdges"
+          :layouts="layouts"
+          :configs="configs"
+          :width="2000"
+          :height="1500"
+          @node-click="handleNodeClick"
+      />
+    </div>
+
     <FamilyInfoModal
-        v-if="showModal"
         :show="showModal"
         :node="selectedNode"
         :children="selectedNodeChildren"
@@ -35,9 +45,14 @@ import FamilyInfoModal from './FamilyInfoModal.vue'
 import { familyData } from './familyData'
 
 export default {
-  components: { VNetworkGraph, FamilyInfoModal },
+  name: 'FamilyTreeLayout',
+  components: {
+    VNetworkGraph,
+    FamilyInfoModal
+  },
   data() {
     return {
+      zoom: 1,
       showModal: false,
       selectedNode: null,
       nodes: familyData.nodes,
@@ -45,31 +60,43 @@ export default {
       layouts: {
         nodes: {}
       },
+      searchQuery: '',
+      relatedNodes: new Set(), // Store IDs of related nodes
       configs: {
+        view: {
+          scalingObjects: true,
+          minZoomLevel: 0.2,
+          maxZoomLevel: 2,
+        },
         node: {
           normal: {
             type: 'circle',
-            radius: 18,
-            color: '#4169E1',
+            radius: 35,
+            color: (node) => {
+              const colors = ['#4169E1', '#1E90FF', '#00BFFF', '#87CEEB', '#87CEFA']
+              return colors[node.generation - 1] || '#4169E1'
+            },
             label: {
               visible: true,
-              fontSize: 20,
+              fontSize: 16,
               color: '#FFFFFF',
               direction: 'center',
               formatter: (node) => `${node.name.split(' ').pop()}\n(${node.birthYear})`
             }
           },
           hover: {
-            scale: 1.2
+            scale: 1.2,
           }
         },
         edge: {
           normal: {
             width: 3,
-            color: '#808080'
+            color: '#808080',
+            linecap: 'round'
           },
           hover: {
-            width: 4
+            width: 4,
+            color: '#404040'
           }
         }
       }
@@ -98,7 +125,7 @@ export default {
       )
     }
   },
-  mounted() {
+  created() {
     this.initializeEdges()
     this.calculateLayout()
   },
@@ -119,6 +146,32 @@ export default {
         }
       })
     },
+    addRelatedNodes(nodeId) {
+      // Add the node itself
+      this.relatedNodes.add(nodeId)
+      const node = this.nodes[nodeId]
+
+      // Add parents
+      if (node.motherId) {
+        const father = Object.values(this.nodes).find(n =>
+            n.spouses && n.spouses.some(s => s.id === node.motherId)
+        )
+        if (father) {
+          this.addRelatedNodes(father.id)
+        }
+      }
+
+      // Add children
+      const children = this.getChildren(nodeId)
+      children.forEach(child => {
+        this.addRelatedNodes(child.id)
+      })
+    },
+    clearSearch() {
+      this.searchQuery = ''
+      this.relatedNodes.clear()
+    },
+    // ... rest of the existing methods remain the same
     handleNodeClick(nodeId) {
       console.log('Node clicked:', nodeId)
       this.selectedNode = this.nodes[nodeId]
@@ -160,30 +213,108 @@ export default {
         generations[node.generation].push(node)
       })
 
-      const levelHeight = 120
+      const levelHeight = 250
+      const nodeSpacing = 200
       Object.entries(generations).forEach(([gen, nodes]) => {
-        const y = parseInt(gen) * levelHeight
-        const width = Math.max(document.body.clientWidth, nodes.length * 200)
-        const startX = (width - (nodes.length - 1) * 200) / 2
+        const y = (parseInt(gen) - 1) * levelHeight + 50
+        const totalWidth = (nodes.length - 1) * nodeSpacing
+        const startX = (2000 - totalWidth) / 2
 
         nodes.forEach((node, index) => {
           this.layouts.nodes[node.id] = {
-            x: startX + index * 200,
-            y
+            x: startX + index * nodeSpacing,
+            y: y
           }
         })
       })
+    },
+    zoomIn() {
+      this.zoom = Math.min(2, this.zoom + 0.1)
+    },
+    zoomOut() {
+      this.zoom = Math.max(0.5, this.zoom - 0.1)
     }
   }
 }
 </script>
 
 <style scoped>
-.tree-wrapper {
+.tree-container {
   width: 100%;
-  height: 800px;
-  overflow: auto;
+  min-height: 100vh;
+  background-color: #FFFFFF;
   position: relative;
+  display: flex;
+  flex-direction: column;
+}
+
+.search-box {
+  position: fixed;
+  top: 20px;
+  left: 20px;
+  z-index: 1000;
+  display: flex;
+  align-items: center;
   background: white;
+  border-radius: 8px;
+  padding: 8px;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+}
+
+.search-box input {
+  border: 1px solid #ddd;
+  padding: 8px 12px;
+  border-radius: 4px;
+  width: 200px;
+  font-size: 14px;
+}
+
+.clear-btn {
+  background: none;
+  border: none;
+  font-size: 20px;
+  color: #666;
+  cursor: pointer;
+  padding: 0 8px;
+}
+
+.clear-btn:hover {
+  color: #333;
+}
+
+/* Rest of the existing styles remain the same */
+.scroll-container {
+  width: 100%;
+  height: 100%;
+  min-height: 100vh;
+  overflow: auto;
+  transform-origin: top left;
+}
+
+.zoom-controls {
+  position: fixed;
+  top: 20px;
+  right: 20px;
+  z-index: 1000;
+  display: flex;
+  gap: 8px;
+}
+
+.zoom-controls button {
+  width: 40px;
+  height: 40px;
+  border: 2px solid #ddd;
+  background: white;
+  border-radius: 8px;
+  cursor: pointer;
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+}
+
+.zoom-controls button:hover {
+  background: #f5f5f5;
 }
 </style>
